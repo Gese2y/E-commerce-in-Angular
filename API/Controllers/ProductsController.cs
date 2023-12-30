5483637d737b8f8d22f510cd7d02f543a098e009
gese <geseseteshome21@gmail.com>
2023-12-31 00:09:34 +0300

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+var products = _context.Products.ToList();
+return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "get single product";
+        }
+        
+    }
+}
