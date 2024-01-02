95f23354be50f2b3e5448c6fee131a0a2b97d09f
gese <geseseteshome21@gmail.com>
2024-01-02 12:20:16 +0300

    secod push

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,31 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+   [ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductRepository _repo;
+
+    public ProductsController(IProductRepository repo)
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        _repo = repo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+        var products = await _repo.GetProductsAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProduct(int id)
+    {
+        var product = await _repo.GetProductByIdAsync(id);
+
+        if (product == null)
         {
-            _context = context;
+            return NotFound(); // or any other appropriate status code
         }
 
-        [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
-        {
-var products = _context.Products.ToList();
-return Ok(products);
-        }
-        [HttpGet("{id}")]
-        public string GetProduct(int id)
-        {
-            return "get single product";
-        }
-        
+        return Ok(product); 
     }
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(){
+        return Ok(await _repo.GetProductBrandssAsync());
+    }
+     [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes(){
+        return Ok(await _repo.GetProductTypesAsync());
+    }
+}
+
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,32 @@
 
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          var host=  CreateHostBuilder(args).Build();
+          using (var scope = host.Services.CreateScope())
+          {
+            var Services= scope.ServiceProvider;
+            var loggerFactory = Services.GetRequiredService<ILoggerFactory>();
+            try{
+                var context = Services.GetRequiredService<StoreContext>();
+                await StoreContextSeed.SeedAsync(context, loggerFactory);
+                await context.Database.MigrateAsync();
+            }
+            catch(Exception ex){
+                var logger= loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex,"An error occured during migration");
+            }
+          }
+          host.Run();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)=>
+  public static IHostBuilder CreateHostBuilder(string[] args)=>
         Host.CreateDefaultBuilder(args).
         ConfigureWebHostDefaults(WebBuilder=>
         {
