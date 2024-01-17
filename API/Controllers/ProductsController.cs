9dacebc52e4281e4e34c5e85f90eb4b58aad5f5e
gese <geseseteshome21@gmail.com>
2024-01-18 00:54:28 +0300

    Errors handling and validation

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,12 +9,15 @@ using Microsoft.AspNetCore.Mvc;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using API.Errors;
+
 
 namespace API.Controllers
 {
-   [ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+//    [ApiController]
+// [Route("api/[controller]")]
+public class ProductsController : BaseApiController
 {
 
   private readonly IGenercRepository<Product> _productRepo;
@@ -42,11 +45,13 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
         var spec = new ProductsWithTypesAndBrandsSpecification(id);
         var product = await _productRepo.GetEntityWithSpec(spec);
-
+if(product == null) return NotFound(new ApiResponse(404));
 return _mapper.Map<Product, ProductToReturnDto>(product);
     }
     [HttpGet("brands")]
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,21 +1,16 @@
 
 using Infrastructure.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.MiddleWare;
+using API.Extensions;
 
 namespace API
 {
     public class Startup
     {
-       
+
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
@@ -28,30 +23,27 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenercRepository<>),(typeof(GenericRepository<>)));
+
             services.AddAutoMapper(typeof(MappingProfile));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x =>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddSwaggerGen(c =>{
-                c.SwaggerDoc("v1", new OpenApiInfo{
-                    Title="API",Version="v1"
-                });
-            });
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c=>c.SwaggerEndpoint(
-                    "/swagger/v1/swagger.json","API v1"
-                ));
-            }
-
+            app.UseMiddleware<ExceptionMiddleWare>();
+            // if (env.IsDevelopment())
+            // {
+            // app.UseDeveloperExceptionPage();
+            app.UseSwaggerDocumentation();
+            // }
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
 
             app.UseRouting();
